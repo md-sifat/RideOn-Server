d4a25740995dfafa91e222d11a18b7314f5769a8
Md Sifat <mdsifat7252@gmail.com>
2025-04-07 22:11:20 +0600

    database collection created

--- index.js
+++ index.js
@@ -19,6 +19,42 @@ const client = new MongoClient(uri, {
 });
 
 
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        const userCollection = client.db(process.env.MONGO_DB_NAME).collection("users");
+        const carCollection = client.db(process.env.MONGO_DB_NAME).collection("cars");
+
+
+
+
+  
+
+       
+     
+
+      
+
+        
+
+
+        
+
+
+
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 
 
 
