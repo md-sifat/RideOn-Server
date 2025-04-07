822529b21293ec49208e08bbfcb6765702af1d73
Md Sifat <mdsifat7252@gmail.com>
2025-04-07 22:04:04 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 require('dotenv').config();
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/?retryWrites=true&w=majority&appName=${process.env.MONGO_APP_NAME}`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+
 
 
 
